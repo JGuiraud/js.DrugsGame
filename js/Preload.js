794158e384ce9e198a410517f4c3d98846e03835
Aymeric <aymeric.gioia@hotmail.fr>
2017-02-22 09:59:33 +0100

    initial commit parce que je suis un boulet

---
+++ js/Preload.js
@@ -0,0 +1,44 @@
+var InfiniteScroller = InfiniteScroller || {};
+
+//loading the game assets
+InfiniteScroller.Preload = function () { };
+
+InfiniteScroller.Preload.prototype = {
+  preload: function () {
+    //show loading screen
+    this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'preloadbar');
+    this.preloadBar.anchor.setTo(0.5);
+    this.preloadBar.scale.setTo(3);
+
+    this.load.setPreloadSprite(this.preloadBar);
+
+    //load game assets
+    this.load.spritesheet('girl', 'assets/images/girl.png', 82, 80, 6);
+    this.load.spritesheet('playerScratch', 'assets/images/pls3.png', 53, 61, 2);
+    this.load.spritesheet('playerDig', 'assets/images/dog_dig.png', 129, 100, 2);
+    this.load.image('ground', 'assets/images/ground.png');
+    // this.load.image('grass', 'assets/images/grass.png');
+    this.load.audio('whine', ['assets/audio/whine.ogg', 'assets/audio/whine.mp3']);
+    this.load.audio('bark', ['assets/audio/bark.ogg', 'assets/audio/bark.mp3']);
+
+    //from http://www.gamedevacademy.org/html5-phaser-tutorial-spacehipster-a-space-exploration-game/
+    this.load.image('mound', 'assets/images/chest2.png');
+
+    //Adapted from https://openclipart.org/detail/6570/flea:
+    this.load.image('pills', 'assets/images/pill2.png');
+    this.load.image('boobs', 'assets/images/boob.png');
+    this.load.image('seringue', 'assets/images/seringue.png');
+    this.load.image('wine', 'assets/images/wine.png');
+    this.load.image('console', 'assets/images/console.png');
+    this.load.image('canabi', 'assets/images/canabi.png');
+
+    //https://openclipart.org/detail/188266/bone:
+    this.load.image('bone', 'assets/images/toys/bone.png');
+
+    //https://openclipart.org/detail/139615/tennis-ball:
+    this.load.image('ball', 'assets/images/toys/tennisball.png');
+},
+create: function () {
+    this.state.start('Game');
+}
+};
